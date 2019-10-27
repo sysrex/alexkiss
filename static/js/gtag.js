76cd9243eed3a5c1af513220839bea6316d6e078
sysrex <alex@sysrex.com>
2019-10-27 12:07:13 +0000

    testing google analytics tag

---
+++ static/js/gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FGYJ12623C"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FGYJ12623C');
+</script>
